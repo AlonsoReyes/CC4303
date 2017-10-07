0909cb4a1e80053809311392497a8942623a06d3
Alonso <areyes.feris@gmail.com>
2017-10-07 00:39:33 -0300

    nothing useful done yet

--- Tarea2/bwcs.c
+++ Tarea2/bwcs.c
@@ -9,9 +9,12 @@
 #include "jsocket6.4.h"
 #include "Data.h"
 #include <pthread.h>
+#include <time.h>
 
-char bwc_buffer[BUFFER_LENGTH]; // buffer de TCP a UDP
+char bwc_buffer[BUFFER_LENGTH + DHDR]; // buffer de TCP a UDP
 char bwss_buffer[BUFFER_LENGTH]; // buffer de UDP a TCP
+char final_bwssbuff[BUFFER_LENGTH + DHDR];
+char final_bwcbuff[BUFFER_LENGTH];
 
 pthread_t bwss_thread; // thread de TCP a UDP
 pthread_t bwc_thread; // thread de UDP a TCP
@@ -22,6 +25,11 @@ char *bwc_port; // puerto de bwc
 int bwc_socket;
 int bwss_socket;
 int ready; // indicara cuando se habra terminado la escritura hacia bwc (retorno del mensaje) para soltar el socket
+
+int ack_bit; //Indica el numero del bit que se espera
+int frame_seq; // Indica el numero del bit que se espera
+int timeout; //Es el timeout que se utilizara
+int received_ack;
 
 
 pthread_mutex_t mutex;
@@ -41,6 +49,36 @@ void DwriteUDP(int cl, char *buf, int l) {
     }
 	fprintf(stderr, "DwriteUDP: %d bytes \n", l);
 }
+
+
+int getNumSeq(unsigned char *buf) {
+    int res=0;
+    int i;
+
+    for(i=DSEQ; i < DHDR; i++)
+        res = (res*10)+(buf[i]-'0');
+
+// fprintf(stderr, "to_int %d <- %c, %c, %c, %c, %c\n", res, buf[0], buf[1], buf[2], buf[3], buf[4]);
+
+    return res;
+}
+
+void copyArray(int fromOrig, int fromFinal, int toFinal char *original, char *final) {
+	int i;
+	for(i = 0; i < (toFinal - fromFinal); i++) {
+		final[i + fromFinal] = original[i + fromOrig];
+	}
+}
+
+void addNumSeq(int frameSeq, char *buf) {
+	int i;
+	int res = frameSeq;
+	for(i = DSEQ; i < DHDR - 1; i++) {
+		buf[i] = (res % 10) + '0';
+        res /= 10;
+	}
+}
+
 
 /* Funcion principal del programa, encargada del proceso de conexion y lanzamiento de los threads */
 int main(int argc, char **argv) {
@@ -66,6 +104,11 @@ int main(int argc, char **argv) {
 		return 1;
     }
 
+    ack_bit = 0; // Parte en el 0
+    frame_seq = 0; // Parte en el 0
+    timeout = 1; // 1 segundo
+    received_ack = 0;
+
     ready = 0; // var. ready: Aun no se ha completado proceso de escritura a bwc
     pthread_mutex_init(&mutex,NULL); // inicializacion mutex
     pthread_cond_init(&cond,NULL); // inicializacion condicion
@@ -84,6 +127,7 @@ int main(int argc, char **argv) {
    del paquete de retorno desde bwss a bwc */
 void *bwss_connect(void *ptr) {
 	int bytes, cnt;
+	int receivedSeqNum;
 
 	/* conexion del socket UDP al puerto de BWSS */
 	if((bwss_socket = j_socket_udp_connect((char *)ptr, bwss_port)) < 0) {
@@ -93,8 +137,22 @@ void *bwss_connect(void *ptr) {
 
 	/* Proceso de lectura y escritura desde socket UDP (bwss) a socket TCP (bwc) */
 	for(bytes=0;; bytes+=cnt) {
-		int size = BUFFER_LENGTH; // largo maximo a leer
+		int size = BUFFER_LENGTH + DHDR; // largo maximo a leer
 	    cnt = read(bwss_socket, bwc_buffer, size); // lectura del socket UDP al buffer correspondiente al cliente
+	    receivedSeqNum = getNumSeq(bwc_buffer);
+	    if(receivedSeqNum == ack_bit) {
+		    if(bwc_buffer[0] == 'A') {
+		    	received_ack = 1;
+		    	continue;
+		    } else if(bwc_buffer[0] == 'D') {
+		    	copyArray(DHDR, 0, cnt - DHDR, bwc_buffer, final_bwcbuff);
+		    	final_bwssbuff[0] = 'A';
+		    	addNumSeq(receivedSeqNum, final_bwssbuff);
+		    	DwriteUDP(bwss_socket, final_bwssbuff, DHDR);
+	 	  	}
+	 	} else {
+	 		continue;
+	 	}
 	    
 	    fprintf(stderr, "ReadUDP: %d bytes\n", cnt);
 		
@@ -102,7 +160,7 @@ void *bwss_connect(void *ptr) {
 		  	break;
 		}
 
-		Dwrite(bwc_socket, bwc_buffer, cnt); // escritura al socket TCP del cliente
+		Dwrite(bwc_socket, final_bwcbuff_buffer, cnt - DHDR); // escritura al socket TCP del cliente
 
     }
     Dwrite(bwc_socket, bwc_buffer, 0); // escritura de EOF al cliente
@@ -141,7 +199,8 @@ void *bwc_connect(void* ptr) {
 
 /*  Funcion encargada de realizar el proceso de lectura y escritura del paquete de retorno desde bwc a bwss  */
 void* connect_client(void *pcl){
-	int bytes, cnt;
+	int bytes, cnt, i;
+	clock_t t;
 	bwc_socket = *((int *)pcl);
     free(pcl);
 
@@ -151,7 +210,28 @@ void* connect_client(void *pcl){
 	for(bytes=0;; bytes+=cnt) {
         cnt = Dread(bwc_socket, bwss_buffer, BUFFER_LENGTH); // lectura del socket bwc al buffer de bwss
         if(cnt <= 0) break; // condicion de quiebre. Lectura de EOF
-        DwriteUDP(bwss_socket, bwss_buffer, cnt); // escritura del buffer de bwss al socket UDP
+
+        // DEBO MODIFICAR BUFFER PARA CONTENER HEADER
+        copyArray(0, DHDR, BUFFER_LENGTH + DHDR, bwss_buffer, final_bwssbuff);
+        final_bwssbuff[DTYPE] = 'D';
+        addNumSeq(frame_seq, final_bwssbuff);
+        ack_bit = frame_seq;
+        frame_seq = (frame_seq + 1) % MAX_SEQ;
+
+        DwriteUDP(bwss_socket, final_bwssbuff, cnt + DHDR); // escritura del buffer de bwss al socket UDP
+        // AGREGAR TIMEOUT Y HACER QUE ESPERE
+        t = 0;
+        do {
+        	while(!received_ack || (((double)t / CLOCKS_PER_SEC) < timeout)) {
+        	t = clock() - t;
+        	}
+        	if(!received_ack) {
+        		DwriteUDP(bwss_socket, final_bwssbuff, cnt + DHDR); 
+        	}
+        } while(!received_ack)
+
+        received_ack = 0;
+        
 	}
 
 	DwriteUDP(bwss_socket, bwss_buffer, 0); // escritura de EOF al socket UDP
